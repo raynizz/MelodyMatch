b39db5e4a4c88e7c98daed6f2b5ad1f5761bd909
Ruslan Tkachenko <ruslantkachenko20006@gmail.com>
2025-10-20 21:54:27 +0300

    feat: add migration
    - all base entities added

--- src/MelodyMatch.EntityFrameworkCore/Migrations/20251020184846_Add_Base_Entities.Designer.cs
+++ src/MelodyMatch.EntityFrameworkCore/Migrations/20251020184846_Add_Base_Entities.Designer.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using MelodyMatch.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Volo.Abp.EntityFrameworkCore;
@@ -13,9 +14,11 @@ using Volo.Abp.EntityFrameworkCore;
 namespace MelodyMatch.Migrations
 {
     [DbContext(typeof(MelodyMatchDbContext))]
-    partial class MelodyMatchDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251020184846_Add_Base_Entities")]
+    partial class Add_Base_Entities
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
