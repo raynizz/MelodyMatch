bbf8049073c6d92191a3d80170e12ad4e3e7c52c
Ruslan Tkachenko <ruslantkachenko20006@gmail.com>
2025-11-04 22:23:16 +0200

    feat: add migration

--- src/MelodyMatch.EntityFrameworkCore/Migrations/20251104195151_Extended_UserProfile_Table.Designer.cs
+++ src/MelodyMatch.EntityFrameworkCore/Migrations/20251104195151_Extended_UserProfile_Table.Designer.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using MelodyMatch.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Volo.Abp.EntityFrameworkCore;
@@ -13,9 +14,11 @@ using Volo.Abp.EntityFrameworkCore;
 namespace MelodyMatch.Migrations
 {
     [DbContext(typeof(MelodyMatchDbContext))]
-    partial class MelodyMatchDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251104195151_Extended_UserProfile_Table")]
+    partial class Extended_UserProfile_Table
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/MelodyMatch.EntityFrameworkCore/Migrations/20251104195151_Extended_UserProfile_Table.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MelodyMatch.Migrations
+{
+    /// <inheritdoc />
+    public partial class Extended_UserProfile_Table : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int[]>(
+                name: "Interests",
+                schema: "public",
+                table: "UserProfiles",
+                type: "integer[]",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int[]>(
+                name: "PreferredGenders",
+                schema: "public",
+                table: "UserProfiles",
+                type: "integer[]",
+                nullable: false,
+                defaultValue: new int[0]);
+
+            migrationBuilder.AddColumn<int>(
+                name: "PreferredMaxAge",
+                schema: "public",
+                table: "UserProfiles",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "PreferredMinAge",
+                schema: "public",
+                table: "UserProfiles",
+                type: "integer",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Interests",
+                schema: "public",
+                table: "UserProfiles");
+
+            migrationBuilder.DropColumn(
+                name: "PreferredGenders",
+                schema: "public",
+                table: "UserProfiles");
+
+            migrationBuilder.DropColumn(
+                name: "PreferredMaxAge",
+                schema: "public",
+                table: "UserProfiles");
+
+            migrationBuilder.DropColumn(
+                name: "PreferredMinAge",
+                schema: "public",
+                table: "UserProfiles");
+        }
+    }
+}
