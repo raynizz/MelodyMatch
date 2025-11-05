f18da664084bfb3032c9318c373d1ae6f80cd51f
Ruslan Tkachenko <ruslantkachenko20006@gmail.com>
2025-11-05 14:47:15 +0200

    refactor: make service internal and rename endpoints

--- src/MelodyMatch.Application/MelodyMatchUser/MelodyMatchUserInternalAppService.cs
+++ src/MelodyMatch.Application/MelodyMatchUser/MelodyMatchUserInternalAppService.cs
@@ -3,11 +3,13 @@ using MelodyMatch.MelodyMatchUser.DTOs;
 using MelodyMatch.MelodyMatchUser.Services;
 using MelodyMatch.Users;
 using Microsoft.Extensions.Configuration;
+using Volo.Abp;
 using Volo.Abp.Application.Services;
 using Volo.Abp.Users;
 
 namespace MelodyMatch.MelodyMatchUser;
 
+[RemoteService(false)]
 public class MelodyMatchUserInternalAppService : ApplicationService, IMelodyMatchUserInternalAppService
 {
     private readonly IUserRepository _repository;
