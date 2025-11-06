7b87205f357f4b43028b0e012ccf29c15ddd0ef3
Ruslan Tkachenko <ruslantkachenko20006@gmail.com>
2025-11-06 13:25:09 +0200

    feat: add migration

---
+++ src/MelodyMatch.EntityFrameworkCore/Migrations/20251105221257_Add_AvatarUrl_To_MelodyMatchUser_Table.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MelodyMatch.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_AvatarUrl_To_MelodyMatchUser_Table : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AvatarUrl",
+                schema: "public",
+                table: "MelodyMatchUsers",
+                type: "text",
+                nullable: true,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AvatarUrl",
+                schema: "public",
+                table: "MelodyMatchUsers");
+        }
+    }
+}
