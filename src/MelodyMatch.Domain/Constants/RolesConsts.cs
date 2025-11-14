9e6b6723f0dad9a03c4a729c45f2914331801242
Ruslan Tkachenko <ruslantkachenko20006@gmail.com>
2025-11-14 20:35:38 +0200

    feat: add role seader

---
+++ src/MelodyMatch.Domain/Constants/RolesConsts.cs
@@ -0,0 +1,14 @@
+namespace MelodyMatch.Constants;
+
+public static class RolesConsts
+{
+    public const string Admin = "admin";
+    
+    public const string Dater = "dater";
+    
+    public const string AdminDescription = "Administrator with full permissions";
+    
+    public const string DaterDescription = "Regular dating user";
+    
+    public static string DescriptionFieldName = "Description";
+}
