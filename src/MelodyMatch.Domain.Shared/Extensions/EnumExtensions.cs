c23e743fb4593ea6a5cb7e8c53bf0ed72d7c22a9
Ruslan Tkachenko <ruslantkachenko20006@gmail.com>
2025-11-22 15:47:45 +0200

    feat: add enum localizator

--- src/MelodyMatch.Domain.Shared/Extensions/EnumExtensions.cs
+++ src/MelodyMatch.Domain.Shared/Extensions/EnumExtensions.cs
@@ -1,6 +1,8 @@
 using System;
 using System.ComponentModel;
 using System.Reflection;
+using Microsoft.Extensions.Localization;
+using MelodyMatch.Localization;
 
 namespace MelodyMatch.Extensions;
 
@@ -22,5 +24,30 @@ public static class EnumExtensions
         var attribute = field.GetCustomAttribute<DescriptionAttribute>();
         
         return attribute?.Description ?? string.Empty;
+    }
+
+    public static string GetLocalizedDescription(this Enum value, IStringLocalizer<MelodyMatchResource> localizer)
+    {
+        if (value == null)
+        {
+            return string.Empty;
+        }
+
+        var enumType = value.GetType();
+        var enumName = enumType.Name;
+        var valueName = value.ToString();
+        
+        // Try to get localized string with key pattern: "Enum:{EnumTypeName}:{EnumValueName}"
+        var localizationKey = $"Enum:{enumName}:{valueName}";
+        var localizedString = localizer[localizationKey];
+        
+        // If localization found, return it
+        if (!localizedString.ResourceNotFound)
+        {
+            return localizedString.Value;
+        }
+        
+        // Fallback to Description attribute
+        return value.GetDescription();
     }
 }
