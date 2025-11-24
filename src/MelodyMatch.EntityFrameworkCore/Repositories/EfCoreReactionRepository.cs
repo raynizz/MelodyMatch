a102d4ead67e251b6e11f999991d8fba279709c9
Ruslan Tkachenko <ruslantkachenko20006@gmail.com>
2025-11-24 22:25:23 +0200

    feat: reowrk logic of excluding user for match and add logic to get like sender profiles

--- src/MelodyMatch.EntityFrameworkCore/Repositories/EfCoreReactionRepository.cs
+++ src/MelodyMatch.EntityFrameworkCore/Repositories/EfCoreReactionRepository.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using MelodyMatch.EntityFrameworkCore;
+using MelodyMatch.Enums.Reactions;
 using MelodyMatch.Reactions;
 using Microsoft.EntityFrameworkCore;
 using Volo.Abp.Domain.Repositories.EntityFrameworkCore;
@@ -40,12 +41,48 @@ public class EfCoreReactionRepository : EfCoreRepository<MelodyMatchDbContext, R
         
     }
     
-    public async Task<List<Guid>> GetReactedUserIdsAsync(Guid fromUserId)
+    public async Task<List<Guid>> GetReactedUserIdsAsync(Guid fromUserId, ReactionType? reactionType = null)
     {
         var dbContext = await GetDbContextAsync();
-        return await dbContext.Reactions
-            .Where(r => r.FromUserId == fromUserId)
+        
+        var reactions = dbContext.Reactions
+            .Where(r => r.FromUserId == fromUserId);
+        
+        if (reactionType != null)
+        {
+            reactions = reactions.Where(r => r.Type == reactionType.Value);
+        }
+        
+        return await reactions
             .Select(r => r.ToUserId)
+            .Distinct()
+            .ToListAsync();
+    }
+
+    public async Task<List<Reaction>> GetLikesForCurrentUserAsync(Guid toUserId)
+    {
+        var dbContext = await GetDbContextAsync();
+        
+        return await dbContext.Reactions
+            .Include(x => x.FromUser)
+                .ThenInclude(x => x.UserProfile)
+                    .ThenInclude(x => x.ProfilePhotos)
+            .Include(x => x.FromUser)
+                .ThenInclude(x => x.IdentityUser)
+            .Where(r => r.ToUserId == toUserId &&
+                        (r.Type == ReactionType.Like || r.Type == ReactionType.LikeWithMessage))
+            .ToListAsync();
+    }
+
+    public async Task<List<Guid>> GetLikerUserIdsForCurrentUserAsync(Guid toUserId)
+    {
+        var dbContext = await GetDbContextAsync();
+        
+        return await dbContext.Reactions
+            .Where(r => r.ToUserId == toUserId &&
+                        (r.Type == ReactionType.Like || r.Type == ReactionType.LikeWithMessage))
+            .Select(r => r.FromUserId)
+            .Distinct()
             .ToListAsync();
     }
 }
